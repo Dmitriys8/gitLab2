3aaef10bc502c3df9b24a85aedf14f25b5e39a88
Dmitriy Yakovlev <cinhrofazatron8@yandex.ru>
2019-10-08 18:02:40 +0300

    Commit4

---
+++ Hello World 2.cs
@@ -0,0 +1,29 @@
+
+//version 1.1
+
+class MyClass{
+    
+    private string name {get; set};
+    public static int numberOfObjects;
+    public static int numberOfObjects2;
+
+    public static int Main(string[] args)
+    {
+        Console.WriteLine('Hello World!!!');
+        Console.ReadLine();
+        return;
+        return 0;
+    }
+
+    private MyClass(){
+        this.numberOfObjects += 1;
+        return;
+    }
+
+    public static int getNumberOfObjects2(){
+        return NumberOfObjects2;
+    }
+
+    public static String test;
+}
+
